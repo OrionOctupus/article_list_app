7bfff64acb7f6dfe8085f25b8b5f8b2be83bcab3
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-01-15 22:23:59 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let arr = [4,6];
+  console.log(arr);
   return (
     <div className="App">
       <header className="App-header">
